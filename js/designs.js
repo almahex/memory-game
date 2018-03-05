38ee442187f7d3941d15c0719cc0710cd97f3aca
Sara Garci <rsaragarci@gmail.com>
2018-03-05 18:29:13 +0100

    [IMP] Several improvements

--- js/designs.js
+++ js/designs.js
@@ -1,9 +1,20 @@
-// let flippedPair = 0;
-// let firstCard;
-// let score = 0;
-
-// All my cards
-const cards = ['❤','❤','☂','☂','❄','❄'];
+const cards = [
+	'fa-umbrella',
+	'fa-umbrella', 
+	'fa-anchor', 
+	'fa-anchor', 
+	'fa-snowflake', 
+	'fa-snowflake',
+	'fa-heart',
+	'fa-heart',
+	'fa-gem',
+	'fa-gem',
+	'fa-bug',
+	'fa-bug',
+	'fa-moon',
+	'fa-moon',
+	'fa-star',
+	'fa-star'];
 let flippedPair = [];
 let totalMoves = 0;
 let flippedCards = 0;
@@ -27,8 +38,10 @@ function setUpBoard() {
 	const fragment = document.createDocumentFragment();
 	for (let i=0; i<shuffledCards.length; i++) {
 		const newElement = document.createElement('div');
+		const innerNewElement = document.createElement('div');
 		newElement.classList.add('card', 'back-card');
-		newElement.textContent += shuffledCards[i];
+		innerNewElement.classList.add('fas', shuffledCards[i], 'fa-7x');
+		newElement.appendChild(innerNewElement);
        	fragment.appendChild(newElement);
     }
     let board = document.getElementsByClassName('board')[0];
@@ -49,9 +62,27 @@ function flipBack(card) {
 	card.classList.replace('front-card', 'back-card');
 }
 
+function restartGame() {
+	boardCards = document.getElementsByClassName('card');
+	for (let i=0; i<boardCards.length; i++) {
+		boardCards[i].classList.remove('front-card');
+		boardCards[i].classList.add('back-card');
+	}
+	flippedPair = [];
+	totalMoves = 0;
+	flippedCards = 0;
+	showMoves();
+}
+
 function youWon() {
-	const message = `Congratulations! You won the game with a total of ${totalMoves} moves!`
+	let message = `Congratulations! You won the game with a total of ${totalMoves} Moves.`;
 	console.log(message);
+    let board = document.getElementsByClassName('board')[0];
+    board.classList.add('win');
+	board.innerHTML = `<p class="win-message">${message}</p></br>
+					   <p class="win-message">Do you want to play again?</p>
+					   <button class="play-again">Play</button>`;
+	return board;
 }
 
 function showMoves() {
@@ -63,8 +94,7 @@ function showMoves() {
 }
 
 function checkCards() {
-	console.log(flippedPair);
-	if (flippedPair[0].textContent === flippedPair[1].textContent) {
+	if (flippedPair[0].innerHTML === flippedPair[1].innerHTML) {
 		flippedPair[0].classList.add('match');
 		flippedPair[1].classList.add('match');
 		console.log("It is a match!");
@@ -72,7 +102,9 @@ function checkCards() {
 		showMoves();
 		flippedCards += 2;
 		if (flippedCards === cards.length) {
-			youWon();
+			setTimeout(function () {
+	    		youWon();
+			}, 2500);
 		} 
 	} else {
 		flippedPair[0].classList.add('fail');
@@ -85,103 +117,48 @@ function checkCards() {
 	}
 	flippedPair.pop();
 	flippedPair.pop();
-};
+}
 
-function getCard(card) {
-	if (card.classList.contains("card")){
-		return card;
-	} else {
+function getElement(element) {
+	if (element.parentNode.nodeName === '#document' || element.parentNode.parentNode.nodeName === '#document') {
 		return false;
+	} else {
+		if (element.classList.contains("card")) {
+			return element;
+		} else if (element.parentNode.classList.contains("card") || element.parentNode.classList.contains("inner-refresh")) {
+			return element.parentNode;
+		} else if (element.parentNode.parentNode.classList.contains("card") || element.parentNode.parentNode.classList.contains("inner-refresh")) {
+			return element.parentNode.parentNode;
+		} else {
+			return false;
+		}
 	}
 }
 
 document.addEventListener('click', function(e) {
-	card = getCard(e.target);
-	if (card != false) {
-		card.classList.remove('fail');
-		if (flippedPair.length === 0) {
-			flipCard(card);
-		} else if (flippedPair.length === 1) {
-			flipCard(card);
-	    	setTimeout(function () {
-	    		checkCards();
-			}, 2000);
+	element = getElement(e.target);
+	let board = document.getElementsByClassName('board')[0];
+	if (element != false && element != undefined && element != null) {
+		if (element.classList.contains('fail') && element.classList.contains('card') && !element.classList.contains('front-card')) {
+			element.classList.remove('fail');
 		}
-	} else {
-		console.log("Please, select a card.");
+		if (flippedPair.length === 0 && element.classList.contains('card') && !element.classList.contains('front-card')) {
+			flipCard(element);
+		} else if (flippedPair.length === 1 && element.classList.contains('card') && !element.classList.contains('front-card')) {
+			flipCard(element);
+		    setTimeout(function () {
+		    	checkCards();
+			}, 2000);
+		} else if (element.classList.contains('inner-refresh')) {
+			board.innerHTML = "";
+			restartGame();
+	    	setUpBoard();
+		}
+	}
+	if (e.target.classList.contains('play-again')) {
+		board.classList.remove('win');
+		board.innerHTML = "";
+		restartGame();
+	    setUpBoard();
 	}
 });
-
-// function checkContent() {
-// 	return this.textContent;
-// };
-
-// function checkCards() {
-// 	const firstCard = document.getElementsByClassName('front-card')[0];
-// 	const secondCard = document.getElementsByClassName('front-card')[1];
-// 	if (firstCard.textContent === secondCard.textContent) {
-// 		console.log("It is a match!");
-// 	}
-// };
-
-// function flipCard(selectedCard) {
-// 	const backCard = selectedCard.getElementsByClassName('back-card')[0];
-// 	const frontCard = selectedCard.getElementsByClassName('front-card')[0];
-// 	backCard.style.display = 'none';
-// 	frontCard.style.animation = 'flipInY 2s';
-// 	frontCard.style.display = 'block';
-// };
-
-// document.addEventListener('click', function (e) {
-// 	const selectedCard = e.path[0];
-// 	if (selectedCard.className === "card"){
-// 		flipCard(selectedCard);
-// 	} else if (selectedCard.className === "front-card" || selectedCard.className === "back-card") {
-// 		flipCard(selectedCard.parentNode);
-// 	} else {
-// 		console.log("this is not a card");
-// 	}
-// 	if (flippedPair === 1) {
-// 		if (firstCard.textContent === selectedCard.getElementsByClassName('front-card')[0].textContent) {
-// 			console.log("It is a match!");
-// //			score += 1;
-// //			restartGame();
-// 		} else {
-// 			console.log("Try again!");
-// 			restartGame();
-// 		}
-// 		flippedPair = 0;
-// 	} else if (flippedPair === 0) {
-// 		if (selectedCard.className === "card"){
-// 			firstCard = document.getElementsByClassName('front-card')[0];
-// 			flippedPair = 1;
-// 		} else if (selectedCard.className === "front-card" || selectedCard.className === "back-card") {
-// 			firstCard = document.getElementsByClassName('front-card')[0];
-// 			flippedPair = 1;
-// 		} else {
-// 			console.log("this is not a card");
-// 		}
-// 	}
-// });
-
-// document.addEventListener('animationend', function () {
-// 	const frontCard = document.getElementsByClassName('front-card')[0];
-// 	frontCard.style.animation = '';
-// });
-
-// function restartGame() {
-// 	const frontCards = document.getElementsByClassName('front-card');
-// 	const backCards = document.getElementsByClassName('back-card');
-// 	for (let i=0; i<backCards.length; i++) {
-// 		frontCards[i].style.display = 'none';
-// 		backCards[i].style.animation = 'flipInY 3s';
-// 		backCards[i].style.display = 'block';
-// 	}
-// 	flippedPair = 0;
-// };
-
-// const refresh = document.getElementsByClassName('refresh')[0];
-
-// refresh.addEventListener('click', function() {
-// 	restartGame();
-// });
